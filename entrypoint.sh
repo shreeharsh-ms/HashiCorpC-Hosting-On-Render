2999382115bc97bbfff6e89bfcc40c740b547797
Shreeharsh <info.shreeharshshivpuje@gmail.com>
2025-10-18 13:35:28 +0530

    m

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 set -e
 
-# Wait for PostgreSQL
-until pg_isready -h "$VAULT_DB_HOST" -p "$VAULT_DB_PORT" -U "$VAULT_DB_USER"; do
+export PGPASSWORD="ZEtRmToZ5qmKpHzrjRbDsBuMQy2CjCzG"
+
+# Wait for PostgreSQL to be ready
+until pg_isready -h dpg-d3piv8jipnbc739v0d60-a.oregon-postgres.render.com -p 5432 -U vault_db_m7gc_user; do
   echo "Waiting for PostgreSQL..."
   sleep 2
 done
 
 echo "PostgreSQL ready. Starting Vault..."
 
-# Start Vault in background
+# Start Vault
 vault server -config=/vault/config/vault-config.hcl &
 VAULT_PID=$!
 
-# Wait for Vault to start listening
-echo "Waiting for Vault to become ready..."
-export VAULT_ADDR="http://127.0.0.1:${PORT}"
+# Wait until Vault is ready
 until vault status >/dev/null 2>&1; do
-  echo "Vault not ready yet..."
   sleep 2
 done
 
-# Initialize if needed
+# Initialize Vault if not initialized
 if ! vault status -format=json | jq -e '.initialized' >/dev/null; then
-  echo "Initializing Vault..."
-  vault operator init -key-shares=1 -key-threshold=1 -format=json > /vault/init.json
-  echo "Vault initialized. Keys and token saved in /vault/init.json"
+    vault operator init -key-shares=1 -key-threshold=1 -format=json > /vault/init.json
+    echo "Vault initialized. Keys and token saved in /vault/init.json"
 fi
 
-# Keep Vault running
 wait $VAULT_PID
